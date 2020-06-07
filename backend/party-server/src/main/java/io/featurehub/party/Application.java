ab8c449088b3c8132ac058d3068a03b57fa7c2bf
Richard Vowles <richard@bluetrainsoftware.com>
2020-06-07 19:07:08 +1200

    Containerised Build + All in one options (#5)
    This commit contains the resolution of the full
    containerised build of the front end and back-ends
    from a Maven build.
    
    The Admin-UI build shells out
    and runs a docker container to build the Admin-UI.
    
    There is a new tile for managing the party server
    and the MR server because they both need to contain
    the admin UI.
    
    The admin-ui builds and installs as a zip image
    and while building the other docker images grab
    that zip image from the repository and build it in.
    
    This image also contains the all-in-one h2 and all-in-one
    postgres (postgres is external so technically not all
    in one).

--- backend/party-server/src/main/java/io/featurehub/party/Application.java
+++ backend/party-server/src/main/java/io/featurehub/party/Application.java
@@ -10,6 +10,7 @@ import cd.connect.lifecycle.LifecycleStatus;
 import io.featurehub.edge.EdgeFeature;
 import io.featurehub.mr.ManagementRepositoryFeature;
 import io.featurehub.mr.filters.CommonConfiguration;
+import io.featurehub.mr.utils.NginxUtils;
 import io.opentracing.contrib.jaxrs2.client.ClientTracingFeature;
 import io.prometheus.client.hotspot.DefaultExports;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -24,7 +25,7 @@ public class Application {
   public static void main(String[] args) {
     System.setProperty("user.timezone", "UTC");
     try {
-      seeIfWeNeedToRunNginx();
+      NginxUtils.seeIfWeNeedToRunNginx();
       new io.featurehub.party.Application().run();
     } catch (Exception e) {
       log.error("failed", e);
@@ -32,16 +33,6 @@ public class Application {
     }
   }
 
-  // get around the issue of running nginx
-  private static void seeIfWeNeedToRunNginx() {
-    if (System.getProperty("run.nginx") != null) {
-      try {
-        new ProcessBuilder("/usr/sbin/nginx").start();
-      } catch (IOException e) {
-        log.error("Requested to start nginx but failed", e);
-      }
-    }
-  }
 
   private void run() throws Exception {
 
--- pipeline/build/base_party/runner.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-/target/nats-server &
-java -jar /target/party-server-1.1-SNAPSHOT.jar -Rio.featurehub.edge.Application -P/etc/common-config/common.properties
