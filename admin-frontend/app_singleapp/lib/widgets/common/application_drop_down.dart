632704165f9215cd2b5316b883e78b73d4b03967
Irina Southwell <irina.southwell@clearpoint.co.nz>
2020-06-14 20:04:24 +1200

    fix app drop-down. Only show stepper to admin users

--- admin-frontend/app_singleapp/lib/widgets/common/application_drop_down.dart
+++ admin-frontend/app_singleapp/lib/widgets/common/application_drop_down.dart
@@ -1,3 +1,4 @@
+import 'package:app_singleapp/api/client_api.dart';
 import 'package:flutter/material.dart';
 import 'package:mrapi/api.dart';
 
@@ -22,31 +23,41 @@ class _ApplicationDropDownState extends State<ApplicationDropDown> {
           color: Colors.white,
           border: Border.all(color: Colors.black87, width: 0.5),
           borderRadius: BorderRadius.all(Radius.circular(2.0))),
-      child: DropdownButtonHideUnderline(
-        child: DropdownButton(
-          isExpanded: true,
-          isDense: true,
-          items: widget.applications != null && widget.applications.isNotEmpty
-              ? widget.applications.map((Application application) {
-                  return DropdownMenuItem<String>(
-                      value: application.id,
-                      child: Text(
-                        application.name,
-                        style: Theme.of(context).textTheme.bodyText2,
-                        overflow: TextOverflow.ellipsis,
-                      ));
-                }).toList()
-              : null,
-          hint: Text('Select application',
-              style: Theme.of(context).textTheme.subtitle2),
-          onChanged: (value) {
-            setState(() {
-              widget.bloc.mrClient.setCurrentAid(value);
-            });
-          },
-          value: widget.bloc.mrClient.getCurrentAid(),
-        ),
-      ),
+      child: StreamBuilder<String>(
+          stream: (widget.bloc.mrClient as ManagementRepositoryClientBloc)
+              .streamValley
+              .currentAppIdStream,
+          builder: (context, snapshot) {
+            return DropdownButtonHideUnderline(
+              child: DropdownButton(
+                isExpanded: true,
+                isDense: true,
+                items: widget.applications != null &&
+                        widget.applications.isNotEmpty
+                    ? widget.applications.map((Application application) {
+                        return DropdownMenuItem<String>(
+                            value: application.id,
+                            child: Text(
+                              application.name,
+                              style: Theme.of(context).textTheme.bodyText2,
+                              overflow: TextOverflow.ellipsis,
+                            ));
+                      }).toList()
+                    : null,
+                hint: Text('Select application',
+                    style: Theme.of(context).textTheme.subtitle2),
+                onChanged: (value) {
+                  setState(() {
+                    widget.bloc.mrClient.setCurrentAid(value);
+                  });
+                },
+                value: widget.applications
+                    .firstWhere((a) => a.id == snapshot.data,
+                        orElse: () => null)
+                    ?.id,
+              ),
+            );
+          }),
     );
   }
 }
