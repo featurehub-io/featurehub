b63848a67ae51a8bf79c0c855c93b1854df7edb6
Richard Vowles <richard@bluetrainsoftware.com>
2020-06-18 21:58:02 +1200

    force the build to clean, still issue with naming

--- admin-frontend/build/build.sh
+++ admin-frontend/build/build.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 cd /opt/app/app
+echo FLUTTER: Cleaning up after last build
+rm -rf build
+flutter clean
 flutter pub get
-echo building deploy_main
+echo FLUTTER: building deploy_main
 flutter analyze
 if test "$?" != "0"; then
   echo "failed"
   exit 1
 fi
 flutter build web --target=lib/deploy_main.dart
-echo finished building
+echo FLUTTER: finished building
 exit 0
