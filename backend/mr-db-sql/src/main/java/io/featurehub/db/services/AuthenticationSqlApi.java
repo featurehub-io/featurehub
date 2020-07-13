fe4ad7c0848291e613136e3f66b81fc270bda6e3
Richard Vowles <richard@bluetrainsoftware.com>
2020-07-14 09:06:32 +1200

    remove need for nulling name

--- backend/mr-db-sql/src/main/java/io/featurehub/db/services/AuthenticationSqlApi.java
+++ backend/mr-db-sql/src/main/java/io/featurehub/db/services/AuthenticationSqlApi.java
@@ -171,12 +171,8 @@ public class AuthenticationSqlApi implements AuthenticationApi {
 
   @Override
   public Person getPersonByToken(String token) {
-    final Person person = convertUtils.toPerson(new QDbPerson().token.eq(token).findOne(),
+    return convertUtils.toPerson(new QDbPerson().token.eq(token).findOne(),
       Opts.empty());
-    if (person.getName() != null && person.getName().isEmpty()) {
-      person.setName(null);
-    }
-    return person;
   }
 
   @Override
