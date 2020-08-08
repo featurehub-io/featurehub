054a571b4265eaef1be7530d68cf627feca6c189
Richard Vowles <richard@bluetrainsoftware.com>
2020-08-08 17:24:02 +1200

    move examples out to own repo, run configs to own repo

--- examples/todo-backend-typescript/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export FEATUREHUB_APP_ENV_URL=http://192.168.86.49:8553/features/default/ce6b5f90-2a8a-4b29-b10f-7f1c98d878fe/VNftuX5LV6PoazPZsEEIBujM4OBqA1Iv9f9cBGho2LJylvxXMXKGxwD14xt2d7Ma3GHTsdsSO8DTvAYF
-npm run start
--- run-configurations/all-in-one-postgres/initdb/initdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-CREATE USER featurehub PASSWORD 'featurehub';
-CREATE DATABASE featurehub;
-GRANT ALL PRIVILEGES ON DATABASE featurehub TO featurehub;
-EOSQL
