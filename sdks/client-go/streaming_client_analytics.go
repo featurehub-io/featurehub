5279d1ed641301d6d66e19554f4f5087fa77c7a9
chris <chris@Profanity.local>
2020-08-16 10:17:28 +1200

    Copying the feature set before submitting analytics events

--- sdks/client-go/streaming_client_analytics.go
+++ sdks/client-go/streaming_client_analytics.go
@@ -3,5 +3,8 @@ package client
 // LogAnalyticsEvent submits analytics events using the client's configured AnalyticsCollector:
 func (c *StreamingClient) LogAnalyticsEvent(action string, other map[string]string) error {
 
-	return c.analyticsCollector.LogEvent(action, other, c.features)
+	// Make a copy of our list of features (in case it changes underneath us):
+	copyOfFeatures := c.features
+
+	return c.analyticsCollector.LogEvent(action, other, copyOfFeatures)
 }
