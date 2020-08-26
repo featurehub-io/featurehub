fd1d1e85703377d08cac4a8ad267b4d1909e3b46
Richard Vowles <richard@bluetrainsoftware.com>
2020-08-26 22:11:46 +1200

    date time matcher updated and specs written

--- backend/sse-strategy-matchers/src/main/java/io/featurehub/strategies/matchers/DateTimeArrayMatcher.java
+++ backend/sse-strategy-matchers/src/main/java/io/featurehub/strategies/matchers/DateTimeArrayMatcher.java
@@ -2,8 +2,9 @@ package io.featurehub.strategies.matchers;
 
 import io.featurehub.sse.model.RolloutStrategyAttribute;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.function.Supplier;
 
 public class DateTimeArrayMatcher implements StrategyMatcher {
   @Override
@@ -11,41 +12,38 @@ public class DateTimeArrayMatcher implements StrategyMatcher {
     DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
     try {
-      LocalDate suppliedDate = LocalDate.from(formatter.parse(suppliedValue));
+      Supplier<LocalDateTime> suppliedDate = () -> LocalDateTime.from(formatter.parse(suppliedValue));
 
       switch (attr.getConditional()) {
         case EQUALS: // all match makes no sense
+        case INCLUDES: // same as equals
           return attr.getValues().stream()
-            .anyMatch(v -> suppliedDate.equals(LocalDate.from(formatter.parse(v.toString()))));
+            .anyMatch(v -> suppliedDate.get().equals(LocalDateTime.from(formatter.parse(v.toString()))));
         case ENDS_WITH:
           return attr.getValues().stream().anyMatch(v -> suppliedValue.endsWith(v.toString()));
         case STARTS_WITH:
           return attr.getValues().stream().anyMatch(v -> suppliedValue.startsWith(v.toString()));
         case GREATER:
           return attr.getValues().stream()
-            .anyMatch(v -> suppliedDate.compareTo(LocalDate.from(formatter.parse(v.toString()))) > 0);
+            .anyMatch(v -> suppliedDate.get().compareTo(LocalDateTime.from(formatter.parse(v.toString()))) > 0);
         case GREATER_EQUALS:
           return attr.getValues().stream()
-            .anyMatch(v -> suppliedDate.compareTo(LocalDate.from(formatter.parse(v.toString()))) >= 0);
+            .anyMatch(v -> suppliedDate.get().compareTo(LocalDateTime.from(formatter.parse(v.toString()))) >= 0);
         case LESS:
           return attr.getValues().stream()
-            .anyMatch(v -> suppliedDate.compareTo(LocalDate.from(formatter.parse(v.toString()))) < 0);
+            .anyMatch(v -> suppliedDate.get().compareTo(LocalDateTime.from(formatter.parse(v.toString()))) < 0);
         case LESS_EQUALS:
           return attr.getValues().stream()
-            .anyMatch(v -> suppliedDate.compareTo(LocalDate.from(formatter.parse(v.toString()))) <= 0);
+            .anyMatch(v -> suppliedDate.get().compareTo(LocalDateTime.from(formatter.parse(v.toString()))) <= 0);
         case NOT_EQUALS:
-          return attr.getValues().stream()
-            .noneMatch(v -> suppliedDate.equals(LocalDate.from(formatter.parse(v.toString()))));
-        case INCLUDES: // same as equals
-          return attr.getValues().stream()
-            .anyMatch(v -> suppliedDate.equals(LocalDate.from(formatter.parse(v.toString()))));
         case EXCLUDES: // same as not_equals
           return attr.getValues().stream()
-            .noneMatch(v -> suppliedDate.equals(LocalDate.from(formatter.parse(v.toString()))));
+            .noneMatch(v -> suppliedDate.get().equals(LocalDateTime.from(formatter.parse(v.toString()))));
         case REGEX:
           return attr.getValues().stream().anyMatch(v -> suppliedValue.matches(v.toString()));
       }
     } catch (Exception ignored) {
+      ignored.printStackTrace();
     }
 
     return false;
---
+++ backend/sse-strategy-matchers/src/test/groovy/io/featurehub/strategies/matchers/DateTimeArrayMatcherSpec.groovy
@@ -0,0 +1,52 @@
+package io.featurehub.strategies.matchers
+
+import io.featurehub.sse.model.RolloutStrategyAttribute
+import io.featurehub.sse.model.RolloutStrategyAttributeConditional
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class DateTimeArrayMatcherSpec extends Specification {
+  DateTimeArrayMatcher matcher
+
+  def setup() {
+    matcher = new DateTimeArrayMatcher()
+  }
+
+  @Unroll
+  def 'date array matcher matches as expected'() {
+    given: 'i create a rollout strategy'
+      def rsi = new RolloutStrategyAttribute().conditional(conditional).values(val)
+    when:
+      def theResult = matcher.match(suppliedValue, rsi)
+    then:
+      theResult == result
+    where:
+      suppliedValue          | conditional                                        | val                                              || result
+      '2019-02-01T01:01:01Z' | RolloutStrategyAttributeConditional.EQUALS         | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || true
+      '2019-02-01T01:01:01Z' | RolloutStrategyAttributeConditional.INCLUDES       | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || true
+      '2019-02-01T01:01:01Z' | RolloutStrategyAttributeConditional.NOT_EQUALS     | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || false
+      '2019-02-01T01:01:01Z' | RolloutStrategyAttributeConditional.EXCLUDES       | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || false
+      '2019-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.EQUALS         | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || false
+      '2019-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.INCLUDES       | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || false
+      '2019-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.NOT_EQUALS     | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || true
+      '2019-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.EXCLUDES       | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || true
+      '2019-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.GREATER        | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || true
+      '2019-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.GREATER_EQUALS | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || true
+      '2019-02-01T01:01:01Z' | RolloutStrategyAttributeConditional.GREATER_EQUALS | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || true
+      '2013-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.GREATER        | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || false
+      '2019-01-01T01:01:01Z' | RolloutStrategyAttributeConditional.GREATER_EQUALS | ['2019-01-02T01:01:01Z', '2019-02-01T01:01:01Z'] || false
+      '2014-02-01T01:01:01Z' | RolloutStrategyAttributeConditional.GREATER_EQUALS | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || false
+      '2019-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.LESS           | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || false
+      '2018-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.LESS           | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || true
+      '2019-02-07T01:01:01Z' | RolloutStrategyAttributeConditional.LESS_EQUALS    | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || false
+      '2019-02-01T01:01:01Z' | RolloutStrategyAttributeConditional.LESS_EQUALS    | ['2019-01-01T01:01:01Z', '2019-02-01T01:01:01Z'] || true
+      '2019-07-06T01:01:01Z' | RolloutStrategyAttributeConditional.REGEX          | ['2019-.*']                                       | true
+      '2017-07-06T01:01:01Z' | RolloutStrategyAttributeConditional.REGEX          | ['2019-.*']                                       | false
+      '2017-03-06T01:01:01Z' | RolloutStrategyAttributeConditional.REGEX          | ['2019-.*', '(.*)-03-(.*)']                       | true
+      '2017-03-06T01:01:01Z' | RolloutStrategyAttributeConditional.STARTS_WITH    | ['2019', '2017']                                  | true
+      '2017-03-06T01:01:01Z' | RolloutStrategyAttributeConditional.STARTS_WITH    | ['2019']                                          | false
+      '2017-03-06T01:01:01Z' | RolloutStrategyAttributeConditional.ENDS_WITH      | [':01Z']                                          | true
+      '2017-03-06T01:01:01Z' | RolloutStrategyAttributeConditional.ENDS_WITH      | ['03', '2017', '01:01']                           | false
+      '2017-03-06T01:01:01Z' | RolloutStrategyAttributeConditional.ENDS_WITH      | ['rubbish']                                       | false
+  }
+}
