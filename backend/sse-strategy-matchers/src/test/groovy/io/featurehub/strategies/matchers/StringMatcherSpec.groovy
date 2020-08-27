f27f8398e4479d1743cf1824d059a936a5c2fb90
Richard Vowles <richard@bluetrainsoftware.com>
2020-08-28 07:53:06 +1200

    minor fix to string matcher spec

--- backend/sse-strategy-matchers/src/test/groovy/io/featurehub/strategies/matchers/StringMatcherSpec.groovy
+++ backend/sse-strategy-matchers/src/test/groovy/io/featurehub/strategies/matchers/StringMatcherSpec.groovy
@@ -6,14 +6,14 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 class StringMatcherSpec  extends Specification {
-  StringArrayMatcher matcher
+  StringMatcher matcher
 
   def setup() {
-    matcher = new StringArrayMatcher()
+    matcher = new StringMatcher()
   }
 
   @Unroll
-  def 'number array matcher works as expected'() {
+  def 'string array matcher works as expected'() {
     given: 'i create a rollout strategy'
       def rsi = new RolloutStrategyAttribute().conditional(conditional).value(val)
     when:
@@ -21,23 +21,23 @@ class StringMatcherSpec  extends Specification {
     then:
       theResult == result
     where:
-      suppliedValue    | conditional                                        | val                          || result
-      'Android'        | RolloutStrategyAttributeConditional.EQUALS | 'Android' || true
-      'Android'        | RolloutStrategyAttributeConditional.EQUALS         | 'android'                  || false
-      'Android'        | RolloutStrategyAttributeConditional.NOT_EQUALS     | 'Android'                  || false
-      'Android'        | RolloutStrategyAttributeConditional.NOT_EQUALS     | 'android'                  || true
-      'ios'            | RolloutStrategyAttributeConditional.INCLUDES       | 'ios' || true
-      'linux'          | RolloutStrategyAttributeConditional.INCLUDES       | 'ios' || false
-      'ios'            | RolloutStrategyAttributeConditional.EXCLUDES       | 'ios' || false
-      'linux'          | RolloutStrategyAttributeConditional.EXCLUDES       | 'android' || true
-      'peaches'        | RolloutStrategyAttributeConditional.GREATER_EQUALS | 'peaches'                  || true
-      'peaches'        | RolloutStrategyAttributeConditional.GREATER_EQUALS | 'zebras'                   || false
-      'peaches'        | RolloutStrategyAttributeConditional.GREATER_EQUALS | 'cream'          || true
-      'actapus (gold)' | RolloutStrategyAttributeConditional.LESS_EQUALS    | 'mammal'                   || true
-      'actapus (gold)' | RolloutStrategyAttributeConditional.LESS           | 'mammal'                   || true
-      'actapus (gold)' | RolloutStrategyAttributeConditional.LESS_EQUALS    | 'aardvark'                 || false
-      'actapus (gold)' | RolloutStrategyAttributeConditional.LESS           | 'aardvark'    || false
-      'actapus (gold)' | RolloutStrategyAttributeConditional.REGEX | '(.*)gold(.*)' | true
-      'actapus (gold)' | RolloutStrategyAttributeConditional.REGEX | '(.*)purple(.*)' | false
+      suppliedValue    | conditional                                        | val             || result
+      'Android'        | RolloutStrategyAttributeConditional.EQUALS         | 'Android'       || true
+      'Android'        | RolloutStrategyAttributeConditional.EQUALS         | 'android'       || false
+      'Android'        | RolloutStrategyAttributeConditional.NOT_EQUALS     | 'Android'       || false
+      'Android'        | RolloutStrategyAttributeConditional.NOT_EQUALS     | 'android'       || true
+      'ios'            | RolloutStrategyAttributeConditional.INCLUDES       | 'ios'           || true
+      'linux'          | RolloutStrategyAttributeConditional.INCLUDES       | 'ios'           || false
+      'ios'            | RolloutStrategyAttributeConditional.EXCLUDES       | 'ios'           || false
+      'linux'          | RolloutStrategyAttributeConditional.EXCLUDES       | 'android'       || true
+      'peaches'        | RolloutStrategyAttributeConditional.GREATER_EQUALS | 'peaches'       || true
+      'peaches'        | RolloutStrategyAttributeConditional.GREATER_EQUALS | 'zebras'        || false
+      'peaches'        | RolloutStrategyAttributeConditional.GREATER_EQUALS | 'cream'         || true
+      'actapus (gold)' | RolloutStrategyAttributeConditional.LESS_EQUALS    | 'mammal'        || true
+      'actapus (gold)' | RolloutStrategyAttributeConditional.LESS           | 'mammal'        || true
+      'actapus (gold)' | RolloutStrategyAttributeConditional.LESS_EQUALS    | 'aardvark'      || false
+      'actapus (gold)' | RolloutStrategyAttributeConditional.LESS           | 'aardvark'      || false
+      'actapus (gold)' | RolloutStrategyAttributeConditional.REGEX          | '(.*)gold(.*)'   | true
+      'actapus (gold)' | RolloutStrategyAttributeConditional.REGEX          | '(.*)purple(.*)' | false
   }
 }
