95d10bf973247b3ed8ea0b3e6ecba32d966e35a4
Richard Vowles <richard@bluetrainsoftware.com>
2020-09-13 19:23:49 +1200

    remove irrelevant file

--- backend/build-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-    exit -1
-fi
-mvn -Ddocker-cloud-build=true -Dbuild.version=$1 -f pom-packages.xml clean install
-
-docker run -p 8085:80 -v ~/tmp/party:/db us.gcr.io/demohub-283022/demohub/party-server:0.0.7
