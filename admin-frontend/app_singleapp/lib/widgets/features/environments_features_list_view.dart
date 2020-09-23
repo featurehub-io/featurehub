950b166a405625e9ce52f229940712756b9b8363
Richard Vowles <richard@bluetrainsoftware.com>
2020-09-24 08:24:02 +1200

    complex cell layout issue across strategies. (#220)

--- admin-frontend/app_singleapp/lib/widgets/features/environments_features_list_view.dart
+++ admin-frontend/app_singleapp/lib/widgets/features/environments_features_list_view.dart
@@ -15,83 +15,75 @@ class EnvironmentsAndFeatureValuesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<String>>(
-        stream: bloc.featureStatusBloc.shownEnvironmentsStream,
-        builder: (context, snapshot) {
-          return StreamBuilder<TabsState>(
-              stream: bloc.currentTab,
-              builder: (context, currentTabSnapshot) {
-                return StreamBuilder<Set<String>>(
-                    stream: bloc.featureCurrentlyEditingStream,
-                    builder: (context, snapshot) {
-                      return Container(
-                        height: bloc.unselectedFeatureCountForHeight +
-                            bloc.selectedFeatureCountForHeight +
-                            headerHeight +
-                            2 +
-                            35,
-                        child: Scrollbar(
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            physics: ClampingScrollPhysics(),
-                            children: [
-                              if (bloc.features.isNotEmpty)
-                                ...bloc.sortedEnvironmentsThatAreShowing
-                                    .map((efv) {
-                                  return Container(
+    return StreamBuilder<TabsState>(
+        stream: bloc.currentTab,
+        builder: (context, currentTabSnapshot) {
+          return StreamBuilder<Set<String>>(
+              stream: bloc.featureCurrentlyEditingStream,
+              builder: (context, snapshot) {
+                final unselHeight = bloc.unselectedFeatureCountForHeight;
+                final selHeight = bloc.selectedFeatureCountForHeight;
+                return Container(
+                  height: unselHeight + selHeight + headerHeight + 2 + 35,
+                  child: Scrollbar(
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      physics: ClampingScrollPhysics(),
+                      children: [
+                        if (bloc.features.isNotEmpty)
+                          ...bloc.sortedEnvironmentsThatAreShowing.map((efv) {
+                            return Container(
 //                                  padding:
 //                                      EdgeInsets.only(left: 1.0, right: 1.0),
+                              width: cellWidth,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Container(
                                     width: cellWidth,
+                                    color: Theme.of(context).primaryColorLight,
+                                    height: headerHeight,
                                     child: Column(
                                       mainAxisAlignment:
-                                          MainAxisAlignment.start,
+                                          MainAxisAlignment.center,
                                       children: [
-                                        Container(
-                                          width: cellWidth,
-                                          color: Theme.of(context)
-                                              .primaryColorLight,
-                                          height: headerHeight,
-                                          child: Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: [
-                                              Text(
-                                                efv.environmentName.toUpperCase(),
-                                                overflow: TextOverflow.ellipsis,
-                                                style: Theme.of(context).textTheme.overline.copyWith(color: Colors.black87, fontSize: 14)
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        ...bloc.features.map((f) {
-                                          return Container(
-                                            decoration: BoxDecoration(
-                                                border: Border(
-                                              bottom: BorderSide(
-                                                  color: Colors.black45,
-                                                  width: 0.5),
-                                              right: BorderSide(
-                                                  color: Colors.black45,
-                                                  width: 0.5),
-                                            )),
-                                            child: FeatureValueCell(
-                                                tabsBloc: bloc,
-                                                feature: f,
-                                                value: efv.features.firstWhere(
-                                                    (fv) => fv.key == f.key,
-                                                    orElse: () => null),
-                                                efv: efv),
-                                          );
-                                        }).toList(),
+                                        Text(efv.environmentName.toUpperCase(),
+                                            overflow: TextOverflow.ellipsis,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .overline
+                                                .copyWith(
+                                                    color: Colors.black87,
+                                                    fontSize: 14)),
                                       ],
                                     ),
-                                  );
-                                })
-                            ],
-                          ),
-                        ),
-                      );
-                    });
+                                  ),
+                                  ...bloc.features.map((f) {
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                          border: Border(
+                                        bottom: BorderSide(
+                                            color: Colors.black45, width: 0.5),
+                                        right: BorderSide(
+                                            color: Colors.black45, width: 0.5),
+                                      )),
+                                      child: FeatureValueCell(
+                                          tabsBloc: bloc,
+                                          feature: f,
+                                          value: efv.features.firstWhere(
+                                              (fv) => fv.key == f.key,
+                                              orElse: () => null),
+                                          efv: efv),
+                                    );
+                                  }).toList(),
+                                ],
+                              ),
+                            );
+                          })
+                      ],
+                    ),
+                  ),
+                );
               });
         });
   }
