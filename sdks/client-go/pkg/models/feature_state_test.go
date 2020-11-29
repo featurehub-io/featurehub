deced7186100dcb9ddaae19f864ebc1f640b43cb
Chrusty <Chris@H>
2020-11-29 14:27:43 +1300

    Some happy unit tests

---
+++ sdks/client-go/pkg/models/feature_state_test.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFeatureStateTest(t *testing.T) {
+
+	// Boolean:
+	featureStateBool := &FeatureState{
+		ID:    "featureStateBool",
+		Type:  TypeBoolean,
+		Value: true,
+	}
+	boolValue, err := featureStateBool.AsBoolean()
+	assert.NoError(t, err)
+	assert.Equal(t, true, boolValue)
+
+	// Raw JSON:
+	featureStateJSON := &FeatureState{
+		ID:    "featureStateJSON",
+		Type:  TypeJSON,
+		Value: `{"is_testy":true}`,
+	}
+	rawJSONValue, err := featureStateJSON.AsRawJSON()
+	assert.NoError(t, err)
+	assert.Equal(t, `{"is_testy":true}`, rawJSONValue)
+
+	// Number:
+	featureStateNumber := &FeatureState{
+		ID:    "featureStateNumber",
+		Type:  TypeNumber,
+		Value: 3.0,
+	}
+	numberValue, err := featureStateNumber.AsNumber()
+	assert.NoError(t, err)
+	assert.Equal(t, 3.0, numberValue)
+
+	// String:
+	featureStateString := &FeatureState{
+		ID:    "featureStateString",
+		Type:  TypeString,
+		Value: "hello, i am a string",
+	}
+	stringValue, err := featureStateString.AsString()
+	assert.NoError(t, err)
+	assert.Equal(t, "hello, i am a string", stringValue)
+}
