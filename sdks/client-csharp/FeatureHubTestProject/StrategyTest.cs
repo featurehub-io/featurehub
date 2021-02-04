c83574bec8c6bf5fa884770dda30866fcaa5e32e
Richard Vowles <richard@bluetrainsoftware.com>
2021-02-05 09:12:03 +1300

    number test

--- sdks/client-csharp/FeatureHubTestProject/StrategyTest.cs
+++ sdks/client-csharp/FeatureHubTestProject/StrategyTest.cs
@@ -39,13 +39,13 @@ namespace FeatureHubTestProject
       feature.Type = FeatureValueType.BOOLEAN;
       var strategy = new RolloutStrategy("id", "name");
       strategy.Value = false;
-      var attr = new RolloutStrategyAttribute();
-      attr.Conditional = RolloutStrategyAttributeConditional.EQUALS;
-      attr.Type = RolloutStrategyFieldType.STRING;
-      attr.FieldName = GetEnumMemberValue(StrategyAttributeWellKnownNames.Country);
-      attr.Values = new List<object> {GetEnumMemberValue(StrategyAttributeCountryName.Turkey)};
+      var ruleset = new RolloutStrategyAttribute();
+      ruleset.Conditional = RolloutStrategyAttributeConditional.EQUALS;
+      ruleset.Type = RolloutStrategyFieldType.STRING;
+      ruleset.FieldName = GetEnumMemberValue(StrategyAttributeWellKnownNames.Country);
+      ruleset.Values = new List<object> {GetEnumMemberValue(StrategyAttributeCountryName.Turkey)};
 
-      strategy.Attributes = new List<RolloutStrategyAttribute> {attr};
+      strategy.Attributes = new List<RolloutStrategyAttribute> {ruleset};
       feature.Strategies = new List<RolloutStrategy> {strategy};
 
       repo.Notify(new List<FeatureState>{feature});
@@ -56,6 +56,49 @@ namespace FeatureHubTestProject
       Assert.AreEqual(false, repo.GetFeature("bool1").WithContext(matchCC).BooleanValue);
       Assert.AreEqual(true, repo.GetFeature("bool1").WithContext(unmatchCC).BooleanValue);
       Assert.AreEqual(true, repo.GetFeature("bool1").BooleanValue);
+    }
+
+    [Test]
+    public void BasicNumberStrategy()
+    {
+      // given: we have a basic number feature with two custom strategies based on age
+      var feature = new FeatureState();
+      feature.Key = "num1";
+      feature.Value = 16;
+      feature.Version = 1;
+      feature.Type = FeatureValueType.NUMBER;
+      var over40Strategy = new RolloutStrategy("id", "name");
+      over40Strategy.Value = 6;
+      var ruleset1 = new RolloutStrategyAttribute();
+      ruleset1.Conditional = RolloutStrategyAttributeConditional.GREATEREQUALS;
+      ruleset1.Type = RolloutStrategyFieldType.NUMBER;
+      ruleset1.FieldName = "age";
+      ruleset1.Values = new List<object> {40};
+
+      over40Strategy.Attributes = new List<RolloutStrategyAttribute> {ruleset1};
+
+      var over20Strategy = new RolloutStrategy("id", "name");
+      over20Strategy.Value = 10;
+      var ruleset2 = new RolloutStrategyAttribute();
+      ruleset2.Conditional = RolloutStrategyAttributeConditional.GREATEREQUALS;
+      ruleset2.Type = RolloutStrategyFieldType.NUMBER;
+      ruleset2.FieldName = "age";
+      ruleset2.Values = new List<object> {20};
+      over20Strategy.Attributes = new List<RolloutStrategyAttribute> {ruleset2};
+
+      feature.Strategies = new List<RolloutStrategy> {over40Strategy, over20Strategy};
+
+      // when: setup repo
+      repo.Notify(new List<FeatureState>{feature});
+
+      var age27 = new IndividualClientContext().Attr("age", "27");
+      var age18 = new IndividualClientContext().Attr("age", "18");
+      var age43 = new IndividualClientContext().Attr("age", "43");
+
+      // then
+      Assert.AreEqual(10, repo.GetFeature("num1").WithContext(age27).NumberValue);
+      Assert.AreEqual(16, repo.GetFeature("num1").WithContext(age18).NumberValue);
+      Assert.AreEqual(6, repo.GetFeature("num1").WithContext(age43).NumberValue);
     }
   }
 }
