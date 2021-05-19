60d3d08ae6046815a760a83f3e2bacb8adf91110
Richard Vowles <richard@bluetrainsoftware.com>
2021-05-19 21:08:04 +1200

    Update SDK with logging and disable backoff
    The SDK has been updated with logging support for diagnosis,
    and backoff has been disabled, as it was causing feature
    drift.

--- sdks/client-csharp/FeatureHubSDK/EventServiceListener.cs
+++ sdks/client-csharp/FeatureHubSDK/EventServiceListener.cs
@@ -16,6 +16,18 @@ namespace FeatureHubSDK
     void Poll();
   }
 
+  public static class FeatureLogging
+  {
+    // Attach event handler to receive Trace level logs
+    public static EventHandler<String> TraceLogger;
+    // Attach event handler to receive Debug level logs
+    public static EventHandler<String> DebugLogger;
+    // Attach event handler to receive Info level logs
+    public static EventHandler<String> InfoLogger;
+    // Attach event handler to receive Error level logs
+    public static EventHandler<String> ErrorLogger;
+  }
+
   public class EventServiceListener : IEdgeService
   {
     private EventSource _eventSource;
@@ -88,14 +100,24 @@ namespace FeatureHubSDK
     public void Init()
     {
       var config = new Configuration(uri: new UriBuilder(_featureHost.Url).Uri,
+        backoffResetThreshold: TimeSpan.MaxValue,
+        delayRetryDuration: TimeSpan.Zero,
         requestHeaders: _featureHost.ServerEvaluation ? BuildContextHeader() : null);
+
+      if (FeatureLogging.InfoLogger != null)
+      {
+        FeatureLogging.InfoLogger(this, $"Opening connection to ${_featureHost.Url}");
+      }
 
       _eventSource = new EventSource(config);
 
-      // _eventSource.Closed += (sender, args) =>
-      // {
-      //   Console.WriteLine("source closed\n");
-      // };
+      if (FeatureLogging.DebugLogger != null)
+      {
+        _eventSource.Closed += (sender, args) =>
+        {
+          FeatureLogging.DebugLogger(this, "source closed");
+        };
+      }
 
       _eventSource.MessageReceived += (sender, args) =>
       {
@@ -104,6 +126,11 @@ namespace FeatureHubSDK
         {
           case "features":
             state = SSEResultState.Features;
+            if (FeatureLogging.TraceLogger != null)
+            {
+              FeatureLogging.TraceLogger(this, "featurehub: fresh feature set received, ready to rumble");
+            }
+
             break;
           case "feature":
             state = SSEResultState.Feature;
@@ -114,12 +141,23 @@ namespace FeatureHubSDK
           case "delete_feature":
             state = SSEResultState.Deletefeature;
             break;
+          case "bye":
+            state = null;
+            if (FeatureLogging.TraceLogger != null)
+            {
+              FeatureLogging.TraceLogger(this, "featurehub: renewing connection process started");
+            }
+
+            break;
           default:
             state = null;
             break;
         }
 
-        // Console.WriteLine($"The state was {state} with value {args.Message.Data}\n");
+        if (FeatureLogging.TraceLogger != null)
+        {
+          FeatureLogging.TraceLogger(this , $"featurehub: The state was {state} with value {args.Message.Data}");
+        }
 
         if (state == null) return;
 
@@ -127,6 +165,11 @@ namespace FeatureHubSDK
 
         if (state == SSEResultState.Failure)
         {
+          if (FeatureLogging.ErrorLogger != null)
+          {
+            FeatureLogging.ErrorLogger(this, "featurehub: received a failure so closing");
+          }
+
           _eventSource.Close();
         }
       };
