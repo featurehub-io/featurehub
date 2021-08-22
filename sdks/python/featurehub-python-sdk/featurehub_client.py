b6168ceadd1a4dbcb9bb24561a68ecb751df6b76
Irina Southwell <irinasouthwell@gmail.com>
2021-08-22 21:34:01 +1200

    improve some tests

--- sdks/python/featurehub-python-sdk/featurehub_client.py
+++ sdks/python/featurehub-python-sdk/featurehub_client.py
@@ -3,19 +3,21 @@ import httpx
 
 class FeatureHubRepository:
     features = {}
-    ready = False
+    ready: bool
 
-    def notify(self, status: str, data):
+    def notify(self, status: str, data: list[dict]):
         if status == 'FEATURES':
-            self.update_features(data)
+            self.__update_features(data)
             self.ready = True
         elif status == 'FAILED':
             self.ready = False
 
-    def update_features(self, data: list[dict]):
-        for feature_apikey in data:
-            for feature in feature_apikey['features']:
-                self.features[feature['key']] = feature
+    def __update_features(self, data: list[dict]):
+        if data:
+            for feature_apikey in data:
+                if feature_apikey:
+                    for feature in feature_apikey['features']:
+                        self.features[feature['key']] = feature
 
     def is_ready(self):
         return self.ready
@@ -36,12 +38,12 @@ class FeatureHubClient:
             self.url = host + '/features?' + '&'.join(map(lambda i: 'apiKeys=' + i, api_keys))
             print(self.url)
             # if clientSideEvaluation:
-            self.check_for_updates()
+            self.__check_for_updates()
 
         else:
             raise RuntimeError("FeatureHubClient initialized without any api keys")
 
-    def check_for_updates(self):
+    def __check_for_updates(self):
         with httpx.Client(http2=True) as client:
             # headers = {'Content-Type': 'application/json'}
             resp = client.get(self.url)
--- sdks/python/featurehub-python-sdk/featurehub_client_test.py
+++ sdks/python/featurehub-python-sdk/featurehub_client_test.py
@@ -1,21 +1,18 @@
 import unittest
+
+import httpx
+import respx
+from httpx import Response
+
 import featurehub_client
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch, Mock
 
 
 class FeatureHubClientTest(unittest.TestCase):
 
-    def test_make_fh_client_object(self):
-        repo = featurehub_client.FeatureHubRepository()
-        fh_client = featurehub_client.FeatureHubClient("http://localhost", ["abc", "123"], repo)
-        # testing
-        # fh_client = featurehub_client.FeatureHubClient("https://kd0gwa.demo.featurehub.io", ["default/190017b3-3e4d-4c68-9805-d52c2b597fe0/hrn6TUohWtUFIvlPCar6RVYkZ5fZH9*3VnmTK5Wp47EQiG6x38n"
-        # ,'default/29517115-da60-4b64-99db-9da017561edd/7NZq23UuFEdmpcwxx7lkKSH0QH1EIS*dmGCHaOErbXyMLfzHCe0'], repo)
-
-        self.assertIsInstance(fh_client, featurehub_client.FeatureHubClient)
-
     def test_is_ready_func(self):
         repo = featurehub_client.FeatureHubRepository()
+        repo.ready = False
         self.assertFalse(repo.is_ready())
         repo.ready = True
         self.assertTrue(repo.is_ready())
@@ -35,33 +32,49 @@ class FeatureHubClientTest(unittest.TestCase):
         with self.assertRaises(RuntimeError):
             featurehub_client.FeatureHubClient("", ["abc"], repo)
 
-    def test_update_features_func(self):
-        repo = featurehub_client.FeatureHubRepository()
-        data = [{'id': '190017b3-3e4d-4c68-9805-d52c2b597fe0', 'features': [{'id': '649b3792-1774-4bd5-b550-973ec6340531', 'key': 'FEATURE_TITLE_TO_UPPERCASE', 'l': True, 'version': 1, 'type': 'BOOLEAN', 'value': False, 'strategies': []},
-                                                                            {'id': '4033e577-1157-4e8f-9f18-b72317e80a57', 'key': 'SUBMIT_COLOR_BUTTON', 'l': False, 'version': 0, 'type': 'STRING', 'strategies': []}]}]
-        repo.update_features(data)
-        self.assertEqual(repo.features, {'FEATURE_TITLE_TO_UPPERCASE': {'id': '649b3792-1774-4bd5-b550-973ec6340531', 'key': 'FEATURE_TITLE_TO_UPPERCASE', 'l': True, 'version': 1, 'type': 'BOOLEAN', 'value': False, 'strategies': []},
-                                         'SUBMIT_COLOR_BUTTON': {'id': '4033e577-1157-4e8f-9f18-b72317e80a57', 'key': 'SUBMIT_COLOR_BUTTON', 'l': False, 'version': 0, 'type': 'STRING', 'strategies': []}})
+    @respx.mock
+    def test_init_fh_client_success(self):
+        data = [{'id': '190017b3-3e4d-4c68-9805-d52c2b597fe0', 'features': [{'id': '649b3792-1774-4bd5-b550-973ec6340531', 'key': 'FEATURE_TITLE_TO_UPPERCASE',
+                                                                             'l': True, 'version': 1, 'type': 'BOOLEAN', 'value': False, 'strategies': []},
+                                                                            {'id': '4033e577-1157-4e8f-9f18-b72317e80a57', 'key': 'SUBMIT_COLOR_BUTTON',
+                                                                             'l': False, 'version': 0, 'type': 'STRING', 'strategies': []}]}]
 
-    def test_update_features_func_empty_list(self):
+        respx.get("http://localhost/features?apiKeys=abc&apiKeys=123&apiKeys=xyz").mock(return_value=Response(200, json=data))
+
         repo = featurehub_client.FeatureHubRepository()
-        data = []
-        repo.update_features(data)
+        featurehub_client.FeatureHubClient("http://localhost", ['abc', '123', 'xyz'], repo)
+        self.assertTrue(repo.ready)
+        self.assertEqual(repo.features, {'FEATURE_TITLE_TO_UPPERCASE': {'id': '649b3792-1774-4bd5-b550-973ec6340531', 'key': 'FEATURE_TITLE_TO_UPPERCASE',
+                                                                        'l': True, 'version': 1, 'type': 'BOOLEAN', 'value': False, 'strategies': []},
+                                         'SUBMIT_COLOR_BUTTON': {'id': '4033e577-1157-4e8f-9f18-b72317e80a57', 'key': 'SUBMIT_COLOR_BUTTON',
+                                                                 'l': False, 'version': 0, 'type': 'STRING', 'strategies': []}})
+
+    @respx.mock
+    def test_init_fh_client_fail(self):
+        respx.get("http://localhost/features?apiKeys=abc&apiKeys=123&apiKeys=xyz").mock(return_value=Response(400))
+        repo = featurehub_client.FeatureHubRepository()
+        featurehub_client.FeatureHubClient("http://localhost", ['abc', '123', 'xyz'], repo)
+        self.assertFalse(repo.ready)
         self.assertEqual(repo.features, {})
 
-    def test_notify_func(self):
+    @respx.mock
+    def test_init_fh_client_empty_features(self):
+        respx.get("http://localhost/features?apiKeys=abc&apiKeys=123&apiKeys=xyz").mock(return_value=Response(200, json=[{}]))
         repo = featurehub_client.FeatureHubRepository()
-        repo.update_features = MagicMock()
-        repo.notify('FEATURES', [{'features': [{'key': 'feature_key'}]}])
+        featurehub_client.FeatureHubClient("http://localhost", ['abc', '123', 'xyz'], repo)
         self.assertTrue(repo.ready)
-        repo.update_features.assert_called()
+        self.assertEqual(repo.features, {})
 
-    def test_notify_func_no_features(self):
+    @respx.mock
+    def test_init_fh_client_empty_features_when_none_set(self):
+        data = [{"id":"29517115-da60-4b64-99db-9da017561edd", "features": []}]
+        respx.get("http://localhost/features?apiKeys=abc&apiKeys=123&apiKeys=xyz").mock(return_value=Response(200, json=data))
         repo = featurehub_client.FeatureHubRepository()
-        repo.update_features = MagicMock()
-        repo.notify('FAILED', [{}])
-        self.assertFalse(repo.ready)
-        repo.update_features.assert_not_called()
+        featurehub_client.FeatureHubClient("http://localhost", ['abc', '123', 'xyz'], repo)
+        self.assertTrue(repo.ready)
+        self.assertEqual(repo.features, {})
+
+
 
 
 if __name__ == '__main__':
