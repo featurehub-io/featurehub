1c73c9db93f5d341ce2bdb004b95ded0c50c4741
Vidya Bhargav <vidya.bhargav@featurehub.io>
2021-09-09 19:37:19 +1200

    Add CurrentTime util methods to common web

--- backend/common-web/src/main/java/io/featurehub/jersey/config/CommonConfiguration.java
+++ backend/common-web/src/main/java/io/featurehub/jersey/config/CommonConfiguration.java
@@ -2,10 +2,14 @@ package io.featurehub.jersey.config;
 
 import cd.connect.openapi.support.OpenApiEnumProvider;
 import io.featurehub.jersey.OffsetDateTimeQueryProvider;
+import io.featurehub.utils.CurrentTime;
+import io.featurehub.utils.CurrentTimeSource;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.core.Configurable;
 import jakarta.ws.rs.core.Feature;
 import jakarta.ws.rs.core.FeatureContext;
 import org.glassfish.jersey.CommonProperties;
+import org.glassfish.jersey.internal.inject.AbstractBinder;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.message.GZipEncoder;
@@ -29,6 +33,13 @@ public class CommonConfiguration implements Feature {
   @Override
   public boolean configure(FeatureContext featureContext) {
     basic(featureContext);
+
+    featureContext.register(new AbstractBinder() {
+      @Override
+      protected void configure() {
+        bind(CurrentTime.class).to(CurrentTimeSource.class).in(Singleton.class);
+      }
+    });
     return true;
   }
 }
