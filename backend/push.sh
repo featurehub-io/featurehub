bb2103738188f701c016592fb680eae1b5dfb907
Richard Vowles <richard@bluetrainsoftware.com>
2021-11-03 21:26:49 +1300

    update tag method

--- backend/push.sh
+++ backend/push.sh
@@ -8,6 +8,8 @@ docker tag featurehub/party-server:$1 featurehub/party-server:latest
 docker tag featurehub/dacha:$1 featurehub/dacha:latest
 docker tag featurehub/mr:$1 featurehub/mr:latest
 docker tag featurehub/edge:$1 featurehub/edge:latest
+docker tag featurehub/party-server-ish:$1 featurehub/party-server-ish:latest
+docker tag featurehub/edge-rest:$1 featurehub/edge-rest:latest
 docker push featurehub/party-server:$1
 docker push featurehub/dacha:$1
 docker push featurehub/mr:$1
@@ -16,6 +18,11 @@ docker push featurehub/party-server:latest
 docker push featurehub/dacha:latest
 docker push featurehub/mr:latest
 docker push featurehub/edge:latest
+docker push featurehub/party-server-ish:latest
+docker push featurehub/party-server-ish:$1
+docker push featurehub/edge-rest:$1
+docker push featurehub/edge-rest:latest
+
 
 
 
