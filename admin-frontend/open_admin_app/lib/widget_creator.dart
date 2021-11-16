519cdc801843eda2c118218b7773bdc3ade7ae48
Irina Southwell <irinasouthwell@gmail.com>
2021-11-16 21:12:55 +1300

    [AdminUI] Display user name as a widget in the appbar  (#588)

--- admin-frontend/open_admin_app/lib/widget_creator.dart
+++ admin-frontend/open_admin_app/lib/widget_creator.dart
@@ -6,6 +6,8 @@ import 'api/client_api.dart';
 class WidgetCreator {
   Widget createSigninWidget(ManagementRepositoryClientBloc client) =>
       SigninWidget(client);
+  Widget orgNameContainer(ManagementRepositoryClientBloc client) =>
+      const SizedBox.shrink();
   List<Widget> extraApplicationMenuItems(
           ManagementRepositoryClientBloc client) =>
       [];
