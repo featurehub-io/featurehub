eb3a2b723073107a3a8a68cb891d50aabe218ce3
Richard Vowles <richard@bluetrainsoftware.com>
2021-12-22 14:10:38 +1300

    allow for an intranet version

--- admin-frontend/build/build.sh
+++ admin-frontend/build/build.sh
@@ -14,6 +14,24 @@ cd app_mr_layer && flutter pub get
 echo "Flutter App will be version: ${BUILD_VERSION}"
 cd ../open_admin_app && echo "final appVersion = '${BUILD_VERSION}';" > lib/version.dart && flutter clean && flutter pub get
 
+function rename_main_dart() {
+  cd build/web
+  MAIN_DATE=`date +"%s"`
+  MAIN="main.dart-$MAIN_DATE.js"
+  if [[ $OSTYPE == 'darwin'* ]]; then
+    sed s/main.dart.js/$MAIN/ index.html > index2.html
+    mv index.html index-old.html
+    mv index2.html index.html
+  else
+    sed -i s/main.dart.js/$MAIN/ index.html
+  fi
+  mv main.dart.js $MAIN
+  if test -f "main.dart.js.map"; then
+    mv main.dart.js.map $MAIN.map
+  fi
+  cd ../..
+}
+
 echo FLUTTER: building deploy_main
 #flutter analyze
 #if test "$?" != "0"; then
@@ -24,17 +42,19 @@ flutter build web --target=lib/deploy_main.dart
 # Downloads WASM locally
 # Temporary solution until https://github.com/flutter/flutter/issues/70101 and 77580 provide a better way
 wasmLocation=$(grep canvaskit-wasm build/web/main.dart.js | cut -d'"' -f2)
+
+rename_main_dart
+mv build build_original
+mkdir -p build/web
+
 echo "Downloading WASM from $wasmLocation"
 curl -o build/web/canvaskit.js "$wasmLocation/canvaskit.js"
 curl -o build/web/canvaskit.wasm "$wasmLocation/canvaskit.wasm"
 flutter build web --dart-define=FLUTTER_WEB_CANVASKIT_URL=/ --target=lib/deploy_main.dart
-cd build/web
-MAIN_DATE=`date +"%s"`
-MAIN="main.dart-$MAIN_DATE.js"
-sed -i s/main.dart.js/$MAIN/ index.html
-mv main.dart.js $MAIN
-if test -f "main.dart.js.map"; then
-  mv main.dart.js.map $MAIN.map
-fi
+
+rename_main_dart
+
+mv build/web build_original/web/intranet
+mv build_original build
 echo FLUTTER: finished building, cleaning
 exit 0
