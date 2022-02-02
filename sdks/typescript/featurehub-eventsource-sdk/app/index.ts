86ada28274c4c52bbe1c1a377bb80724c373a3d6
Irina Southwell <irinasouthwell@gmail.com>
2022-02-02 21:37:31 +1300

    remove JS Sdks and all the relevant stuff due to the migration into its own repo (#693)

--- sdks/typescript/featurehub-eventsource-sdk/app/index.ts
+++
@@ -1,8 +0,0 @@
-import { EdgeFeatureHubConfig } from 'featurehub-repository';
-import { FeatureHubEventSourceClient } from './featurehub_eventsource';
-
-EdgeFeatureHubConfig.defaultEdgeServiceSupplier = (repo, cfg) =>
-  new FeatureHubEventSourceClient(cfg, repo);
-
-export * from './featurehub_eventsource';
-export * from 'featurehub-repository';
--- sdks/typescript/featurehub-javascript-node-sdk/README.md
+++
@@ -1,1 +0,0 @@
-../featurehub-javascript-client-sdk/README.md
