8b4881695e570e62d89561e9cd55001132100f96
Irina Southwell <irinasouthwell@gmail.com>
2022-02-13 13:20:23 +1300

    add a padding

--- admin-frontend/open_admin_app/lib/widgets/features/environments_features_list_view.dart
+++ admin-frontend/open_admin_app/lib/widgets/features/environments_features_list_view.dart
@@ -25,81 +25,82 @@ class EnvironmentsAndFeatureValuesListView extends StatelessWidget {
               builder: (context, snapshot) {
                 final unselHeight = bloc.unselectedFeatureCountForHeight;
                 final selHeight = bloc.selectedFeatureCountForHeight;
-                return SizedBox(
-                  height: unselHeight + selHeight + headerHeight,
-                  child: ScrollConfiguration(
-                    behavior: CustomScrollBehavior(),
-                    child: Scrollbar(
-                      controller: controller,
-                      child: ListView(
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: SizedBox(
+                    height: unselHeight + selHeight + headerHeight,
+                    child: ScrollConfiguration(
+                      behavior: CustomScrollBehavior(),
+                      child: Scrollbar(
                         controller: controller,
-                        scrollDirection: Axis.horizontal,
-                        physics: const ClampingScrollPhysics(),
-                        children: [
-                          if (bloc.features.isNotEmpty)
-                            ...bloc.sortedEnvironmentsThatAreShowing.map((efv) {
-                              return SizedBox(
-//                                  padding:
-//                                      EdgeInsets.only(left: 1.0, right: 1.0),
-                                width: cellWidth,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      width: cellWidth,
-                                      color: Theme.of(context).highlightColor,
-                                      height: headerHeight,
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(efv.environmentName!.toUpperCase(),
-                                              overflow: TextOverflow.ellipsis,
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .overline!
-                                                  .copyWith(
-                                                      color: Theme.of(context)
-                                                                  .brightness ==
-                                                              Brightness.light
-                                                          ? Colors.black87
-                                                          : null,
-                                                      fontSize: 14)),
-                                        ],
+                        child: ListView(
+                          controller: controller,
+                          scrollDirection: Axis.horizontal,
+                          physics: const ClampingScrollPhysics(),
+                          children: [
+                            if (bloc.features.isNotEmpty)
+                              ...bloc.sortedEnvironmentsThatAreShowing.map((efv) {
+                                return SizedBox(
+                                  width: cellWidth,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        width: cellWidth,
+                                        color: Theme.of(context).highlightColor,
+                                        height: headerHeight,
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(efv.environmentName!.toUpperCase(),
+                                                overflow: TextOverflow.ellipsis,
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .overline!
+                                                    .copyWith(
+                                                        color: Theme.of(context)
+                                                                    .brightness ==
+                                                                Brightness.light
+                                                            ? Colors.black87
+                                                            : null,
+                                                        fontSize: 14)),
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                    ...bloc.features.map((f) {
-                                      return Container(
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                          bottom: BorderSide(
-                                              color: Theme.of(context)
-                                                  .buttonTheme
-                                                  .colorScheme!
-                                                  .onSurface
-                                                  .withOpacity(0.12),
-                                              width: 1.0),
-                                          right: BorderSide(
-                                              color: Theme.of(context)
-                                                  .buttonTheme
-                                                  .colorScheme!
-                                                  .onSurface
-                                                  .withOpacity(0.12),
-                                              width: 1.0),
-                                        )),
-                                        child: FeatureValueCell(
-                                            tabsBloc: bloc,
-                                            feature: f,
-                                            value: efv.features.firstWhereOrNull(
-                                                (fv) => fv.key == f.key),
-                                            efv: efv),
-                                      );
-                                    }).toList(),
-                                  ],
-                                ),
-                              );
-                            })
-                        ],
+                                      ...bloc.features.map((f) {
+                                        return Container(
+                                          decoration: BoxDecoration(
+                                              border: Border(
+                                            bottom: BorderSide(
+                                                color: Theme.of(context)
+                                                    .buttonTheme
+                                                    .colorScheme!
+                                                    .onSurface
+                                                    .withOpacity(0.12),
+                                                width: 1.0),
+                                            right: BorderSide(
+                                                color: Theme.of(context)
+                                                    .buttonTheme
+                                                    .colorScheme!
+                                                    .onSurface
+                                                    .withOpacity(0.12),
+                                                width: 1.0),
+                                          )),
+                                          child: FeatureValueCell(
+                                              tabsBloc: bloc,
+                                              feature: f,
+                                              value: efv.features.firstWhereOrNull(
+                                                  (fv) => fv.key == f.key),
+                                              efv: efv),
+                                        );
+                                      }).toList(),
+                                    ],
+                                  ),
+                                );
+                              })
+                          ],
+                        ),
                       ),
                     ),
                   ),
