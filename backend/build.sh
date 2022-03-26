bfada1accd0b5f9dc1df15411dc0e334d057185f
Richard Vowles <richard@bluetrainsoftware.com>
2022-03-26 18:52:09 +1300

    [internal] upgrade ebean to 12.16 - Java 11 only (#754)

--- backend/build.sh
+++ backend/build.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]
 else
   VERSION=$1
 fi
-if [[ $VERSION != *"RC"* ]]; then
+if [[ $VERSION == *"RC"* ]]; then
   echo "Is RC, not also tagging latest"
 else
   echo "Is not RC, tagging latest"
