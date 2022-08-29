b26bb97eef2558052b27140f13f9670bcd33c8ab
Richard Vowles <richard@bluetrainsoftware.com>
2022-08-29 21:42:34 +1200

    test fixes

--- backend/common-web/src/main/kotlin/io/featurehub/health/CommonFeatureHubFeatures.kt
+++ backend/common-web/src/main/kotlin/io/featurehub/health/CommonFeatureHubFeatures.kt
@@ -6,15 +6,16 @@ import cd.connect.openapi.support.ReturnStatusContainerResponseFilter
 import io.featurehub.info.ApplicationVersionFeatures
 import io.featurehub.jersey.config.CommonConfiguration
 import io.featurehub.jersey.config.EndpointLoggingListener
-import io.featurehub.utils.*
+import io.featurehub.utils.ConfigInjectionResolver
+import io.featurehub.utils.CurrentTime
+import io.featurehub.utils.CurrentTimeSource
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import jakarta.ws.rs.core.Feature
 import jakarta.ws.rs.core.FeatureContext
-import jakarta.ws.rs.core.GenericType
 import org.glassfish.hk2.api.ServiceLocator
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities
-import org.glassfish.jersey.internal.inject.*
+import org.glassfish.jersey.internal.inject.AbstractBinder
 import org.glassfish.jersey.server.ServerProperties
 
 
@@ -40,9 +41,6 @@ class CommonFeatureHubFeatures @Inject constructor(private val locator: ServiceL
         bind(CurrentTime::class.java).to(CurrentTimeSource::class.java).`in`(
           Singleton::class.java
         )
-
-//        val boundConfigType = object: GenericType<InjectionResolver<FeatureHubConfig>>(){}
-//        bind(ConfigInjectionResolver::class.java).to(boundConfigType).`in`(Singleton::class.java)
         bind(ConfigInjectionResolver())
       }
     })
