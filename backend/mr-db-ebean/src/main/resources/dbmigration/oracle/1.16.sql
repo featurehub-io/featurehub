a670be698576e77ec1106c133fc4bf02071b5de9
Richard Vowles <richard@bluetrainsoftware.com>
2022-10-05 22:25:22 +1300

    oracle cannot handle clobs in queries

--- backend/mr-db-ebean/src/main/resources/dbmigration/oracle/1.16.sql
+++ backend/mr-db-ebean/src/main/resources/dbmigration/oracle/1.16.sql
@@ -1,3 +1,3 @@
 -- apply alter tables
-alter table fh_environment add u_env_inf clob;
-alter table fh_environment add m_env_inf clob;
+alter table fh_environment add u_env_inf varchar(4000);
+alter table fh_environment add m_env_inf varchar(4000);
