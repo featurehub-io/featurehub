93421e9378712e48d076c6b59948d31d45a6ccfd
Alex Deng <alex.deng@featurehub.io>
2023-04-02 19:24:34 +1200

    Remove old API docs bucket

--- infra/api-bucket/index.ts
+++ infra/api-bucket/index.ts
@@ -4,22 +4,6 @@ import * as gcp from "@pulumi/gcp";
 const config = new pulumi.Config()
 const bucketRegion = config.get("bucketRegion");
 
-const bucket = new gcp.storage.Bucket("featurehub-openapi-bucket", {
-    name: "api.dev.featurehub.io",
-    location: bucketRegion,
-    forceDestroy: config.get("bucketDestroy") === "true",
-});
-const publicRule = new gcp.storage.DefaultObjectAccessControl("featurehub-openapi-bucket-rule", {
-    bucket: bucket.name,
-    role: "READER",
-    entity: "allUsers",
-});
-const fhOwner = new gcp.storage.DefaultObjectAccessControl("featurehub-openapi-bucket-owner-rule", {
-    bucket: bucket.name,
-    role: "OWNER",
-    entity: "allAuthenticatedUsers",
-});
-
 const apiDocsBucket = new gcp.storage.Bucket("featurehub-openapi-docs-bucket", {
   name: "featurehubapidocs",
   location: bucketRegion,
