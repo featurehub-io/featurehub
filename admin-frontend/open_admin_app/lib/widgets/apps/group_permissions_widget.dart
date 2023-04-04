d62f58d0f4800ea2f1d17a1f774849fed9c3fd70
Richard Vowles <richard@bluetrainsoftware.com>
2023-04-04 19:05:31 +1200

    [internal/feature-perm-fixup] Feature perms not remembering past 1st
    When you update a feature permission for a group, and then try
    and update again, you get the wrong permissions. This corrects
    the issue.
    
    Fixes #965

--- admin-frontend/open_admin_app/lib/widgets/apps/group_permissions_widget.dart
+++ admin-frontend/open_admin_app/lib/widgets/apps/group_permissions_widget.dart
@@ -156,6 +156,11 @@ class _AdminFeatureRole {
 
   @override
   int get hashCode => id.hashCode;
+
+  @override
+  String toString() {
+    return "id: ${id}, name: ${name}, roles: ${roles}";
+  }
 }
 
 final _adminFeatureRoles = [
@@ -285,12 +290,12 @@ class _GroupPermissionDetailState extends State<_GroupPermissionDetailWidget> {
                         keepCase: true,
                       ),
                       FHFlatButton(
-                          onPressed: () {
+                          onPressed: () async {
                             final newList = <EnvironmentGroupRole>[];
                             newEnvironmentRoles.forEach((key, value) {
                               newList.add(value);
                             });
-                            var newGroup = groupSnapshot.data!.group;
+                            var newGroup = currentGroup!;
                             newGroup.environmentRoles = newList;
                             if (adminFeatureRole != null && originalAdminFeatureRole != null && originalAdminFeatureRole?.id != adminFeatureRole?.id) {
                               replaceGroupRoles(newGroup, applicationId!, originalAdminFeatureRole!, adminFeatureRole!);
@@ -298,9 +303,13 @@ class _GroupPermissionDetailState extends State<_GroupPermissionDetailWidget> {
                             widget.bloc
                                 .updateGroupWithEnvironmentRoles(
                                     newGroup.id, newGroup)
-                                .then((group) => widget.bloc.mrClient
+                                .then((group) {
+                                  currentGroup = group;
+                                  originalAdminFeatureRole = _discoverAdminRoleType(currentGroup!, widget.bloc.applicationId!);
+                                  widget.bloc.mrClient
                                     .addSnackbar(
-                                        Text("Group '${group.name}' updated!")))
+                                        Text("Group '${group.name}' updated!"));
+                                })
                                 .catchError((e, s) {
                               widget.bloc.mrClient.dialogError(e, s);
                             });
@@ -414,7 +423,9 @@ class _GroupPermissionDetailState extends State<_GroupPermissionDetailWidget> {
   void replaceGroupRoles(Group newGroup, String appId, _AdminFeatureRole originalAdminFeatureRole, _AdminFeatureRole adminFeatureRole) {
      final agr = newGroup.applicationRoles.firstWhereOrNull((appGroupRole) => appGroupRole.applicationId == appId);
      if (agr != null) {
+       // these are the roles they have
        final roles = [...agr.roles];
+       // if the new feature
        roles.removeWhere((role) => originalAdminFeatureRole.roles.contains(role) || adminFeatureRole.roles.contains(role) );
        roles.addAll(adminFeatureRole.roles);
        agr.roles = roles;
