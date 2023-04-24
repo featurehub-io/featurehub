b635b82d8b55c7799f8a1388568677730993d3ac
Richard Vowles <richard@bluetrainsoftware.com>
2023-04-24 19:57:54 +1200

    build dart layer more fully sorted

--- e2e/make-image.sh
+++ e2e/make-image.sh
@@ -2,4 +2,6 @@
 if [[ ! -L "app_mr_layer" ]]; then
   ln -s ../admin-frontend/app_mr_layer app_mr_layer
 fi
+cd app_mr_layer && mvn generate-sources
+cd ..
 tar -ch . | docker build -t featurehub/e2e-dart:1.0 -
