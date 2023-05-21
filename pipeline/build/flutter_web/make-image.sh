acf7cc42b0284a6ff7ef8de7b19df7b7f1bed4ea
Richard Vowles <richard@bluetrainsoftware.com>
2023-05-21 17:57:12 +1200

    [internal/flutter-build-image] Upgrade to 3.10.1 (#1003)
    Brings improvements in UI loading speed among
    others.
    
    Co-authored-by: Irina Southwell <irinasouthwell@gmail.com>

--- pipeline/build/flutter_web/make-image.sh
+++ pipeline/build/flutter_web/make-image.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build -t featurehub/flutter_web:1.10 .
+docker build -t featurehub/flutter_web:1.11 .
+docker tag featurehub/flutter_web:1.11 us-central1-docker.pkg.dev/demohub-283022/demohub/build-images/flutter-build:1.11
+
