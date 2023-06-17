143c183204fa31c00a74ca45bedd68737b57e7f6
Richard Vowles <richard@bluetrainsoftware.com>
2023-06-18 09:32:49 +1200

    fixup migrations

--- backend/mr-db-ebean/src/main/resources/dbmigration/oracle/1.21.sql
+++ backend/mr-db-ebean/src/main/resources/dbmigration/oracle/1.21.sql
@@ -14,13 +14,10 @@ update fh_service_account set name = '' where name is null;
 
 update fh_service_account set description = '' where description is null;
 
-update fh_service_account set fk_sdk_person = '' where fk_sdk_person is null;
-
 update fh_service_account set api_key_client_eval = '' where api_key_client_eval is null;
 -- apply alter tables
 alter table fh_service_account modify name not null;
 alter table fh_service_account modify description not null;
-alter table fh_service_account modify fk_sdk_person not null;
 alter table fh_service_account modify api_key_client_eval not null;
 -- apply post alter
 alter table fh_service_account add constraint idx_service_name unique  (fk_portfolio_id,name);
--- backend/mr-db-ebean/src/main/resources/dbmigration/postgres/1.21.sql
+++ backend/mr-db-ebean/src/main/resources/dbmigration/postgres/1.21.sql
@@ -5,13 +5,10 @@ update fh_service_account set name = '' where name is null;
 
 update fh_service_account set description = '' where description is null;
 
-update fh_service_account set fk_sdk_person = '' where fk_sdk_person is null;
-
 update fh_service_account set api_key_client_eval = '' where api_key_client_eval is null;
 -- apply alter tables
 alter table fh_service_account alter column name set not null;
 alter table fh_service_account alter column description set not null;
-alter table fh_service_account alter column fk_sdk_person set not null;
 alter table fh_service_account alter column api_key_client_eval set not null;
 -- apply post alter
 alter table fh_service_account add constraint idx_service_name unique  (fk_portfolio_id,name);
