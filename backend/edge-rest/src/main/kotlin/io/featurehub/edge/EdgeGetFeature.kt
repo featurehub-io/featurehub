1126cace29eaa77cb662ce826233a7f18d91d472
Richard Vowles <richard@bluetrainsoftware.com>
2023-08-28 18:59:30 +1200

    surface api changes properly

--- backend/edge-rest/src/main/kotlin/io/featurehub/edge/EdgeGetFeature.kt
+++ backend/edge-rest/src/main/kotlin/io/featurehub/edge/EdgeGetFeature.kt
@@ -3,6 +3,7 @@ package io.featurehub.edge
 import io.featurehub.dacha.api.DachaClientServiceRegistry
 import io.featurehub.edge.db.sql.DbDachaCacheSqlApi
 import io.featurehub.edge.db.sql.DbFeatureUpdateProcessor
+import io.featurehub.edge.resources.Edge2Resource
 import io.featurehub.edge.resources.EdgeResource
 import io.featurehub.edge.rest.FeatureUpdatePublisher
 import jakarta.inject.Singleton
@@ -14,6 +15,7 @@ class EdgeGetFeature : Feature {
   override fun configure(context: FeatureContext): Boolean {
     context.register(EdgeCommonFeature::class.java)
     context.register(EdgeResource::class.java)
+    context.register(Edge2Resource::class.java)
     context.register(object: AbstractBinder() {
       override fun configure() {
         bind(DbDachaCacheSqlApi::class.java).to(DachaClientServiceRegistry::class.java).`in`(Singleton::class.java)
