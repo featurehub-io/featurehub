8b0b83cc3e41d0e32a62bfce450b8216c9fcd754
Richard Vowles <richard@bluetrainsoftware.com>
2023-08-28 21:23:40 +1200

    Allow NATS health check to be disabled (#1046)
    If this is set to true, then it will prevent NATS going down
    from taking FH down as well.
    
    Fixes #1045

--- backend/eventing-nats/src/main/kotlin/io/featurehub/publish/NATSHealthSource.kt
+++ backend/eventing-nats/src/main/kotlin/io/featurehub/publish/NATSHealthSource.kt
@@ -1,15 +1,26 @@
 package io.featurehub.publish
 
 import io.featurehub.health.HealthSource
+import io.featurehub.utils.FallbackPropertyConfig
 import io.nats.client.Connection
 import jakarta.inject.Inject
 
 class NATSHealthSource
   @Inject
   constructor(val natsSource: NATSSource) : HealthSource {
+    val natsDisabled: Boolean
+
+  init {
+    // check if we want to opt-out of nats being on the healthcheck
+    natsDisabled = FallbackPropertyConfig.getConfig("nats.healthcheck.disabled", "false") == "true"
+  }
 
   override val healthy: Boolean
     get() {
+      if (natsDisabled) {
+        return true
+      }
+
       val status = natsSource.connection.status;
       return status == Connection.Status.CONNECTED || status == Connection.Status.CONNECTING || status == Connection.Status.RECONNECTING
     }
