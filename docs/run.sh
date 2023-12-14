5f27c6658e56dda1c39ae3123e6d11a5cc9b495a
Richard Vowles <richard@bluetrainsoftware.com>
2023-12-15 12:47:03 +1300

    more discussion around SDK resilience

--- docs/run.sh
+++ docs/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo reminder, to use this against "local" builds you must push your changes to your branch and change the antora-playbook.yml to specify that branch instead of main
 rm -rf .cache build
 #docker run -u $(id -u) -v $PWD:/antora:Z --rm -t antora/antora /bin/sh -c "yarn global add @antora/lunr-extension && antora --cache-dir=./.cache/antora antora-playbook.yml"
 docker run -u 0:0 -v $PWD:/antora:Z --rm -t antora/antora /bin/sh -c "yarn global add @antora/lunr-extension && antora --cache-dir=./.cache/antora antora-playbook.yml"
