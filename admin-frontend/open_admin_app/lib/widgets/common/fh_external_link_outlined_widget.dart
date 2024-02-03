26b3d8ecdb5f77384b903ee86d4cf4f07f1f6c79
Irina Southwell <irinasouthwell@gmail.com>
2024-02-04 12:03:48 +1300

    add docs for slack integration add button to Slack FeatureHub bot app installation

---
+++ admin-frontend/open_admin_app/lib/widgets/common/fh_external_link_outlined_widget.dart
@@ -0,0 +1,36 @@
+import 'dart:html'; // ignore: avoid_web_libraries_in_flutter
+
+import 'package:flutter/material.dart';
+
+class FHExternalLinkOutlinedWidget extends StatelessWidget {
+  final String tooltipMessage;
+  final String label;
+  final String link;
+  final Icon icon;
+
+  const FHExternalLinkOutlinedWidget(
+      {Key? key,
+      required this.tooltipMessage,
+      required this.label,
+      required this.link,
+      required this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Tooltip(
+        message: tooltipMessage,
+        child: OutlinedButton.icon(
+          icon: Text(
+            label,
+          ),
+          onPressed: () {
+            window.open(link, 'new tab');
+          },
+          label: icon,
+        ),
+      ),
+    ]);
+  }
+}
