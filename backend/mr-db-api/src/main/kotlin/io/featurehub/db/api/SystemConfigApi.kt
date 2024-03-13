7ac7088427101bafb0f3b41d98a8f3b9430320ad
Richard Vowles <richard@bluetrainsoftware.com>
2024-03-13 20:54:27 +1300

    turn off the system config by default

--- backend/mr-db-api/src/main/kotlin/io/featurehub/db/api/SystemConfigApi.kt
+++ backend/mr-db-api/src/main/kotlin/io/featurehub/db/api/SystemConfigApi.kt
@@ -2,6 +2,7 @@ package io.featurehub.db.api
 
 import io.featurehub.mr.model.SystemConfig
 import io.featurehub.mr.model.UpdatedSystemConfig
+import io.featurehub.utils.FallbackPropertyConfig
 import java.util.*
 
 interface SystemConfigApi {
@@ -16,4 +17,8 @@ interface SystemConfigApi {
 
   @Throws(NoSuchKeyException::class)
   fun decryptSystemConfig(key: String, mapKey: String?): String?
+
+  companion object {
+    val systemConfigEnabled = FallbackPropertyConfig.getConfig("system-config.enabled","false") == "true"
+  }
 }
