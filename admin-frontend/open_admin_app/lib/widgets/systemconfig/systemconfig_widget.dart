75cf9f321936f235adec259344575470fd8e03ed
Irina Southwell <irinasouthwell@gmail.com>
2024-08-21 11:11:28 +1200

    add message

--- admin-frontend/open_admin_app/lib/widgets/systemconfig/systemconfig_widget.dart
+++ admin-frontend/open_admin_app/lib/widgets/systemconfig/systemconfig_widget.dart
@@ -2,6 +2,7 @@ import 'package:bloc_provider/bloc_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:mrapi/api.dart';
 import 'package:open_admin_app/common/ga_id.dart';
+import 'package:open_admin_app/widgets/common/fh_external_link_widget.dart';
 import 'package:open_admin_app/widgets/common/fh_header.dart';
 import 'package:open_admin_app/widgets/common/fh_loading_error.dart';
 import 'package:open_admin_app/widgets/common/fh_loading_indicator.dart';
@@ -47,19 +48,36 @@ class SystemConfigPanel extends StatelessWidget {
                     Card(child: SiteSystemConfigWidget(knownConfigs: config))
                   ],
                 ),
-              if (config.any((cfg) => cfg.key == 'slack.enabled'))
-                ExpansionTile(
-                  title: SelectableText(
-                    'Slack Configuration',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  subtitle: Text('Enable FeatureHub to send Slack messages',
-                      style: Theme.of(context).textTheme.titleSmall),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  children: [
+              ExpansionTile(
+                title: SelectableText(
+                  'Slack Configuration',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                subtitle: Text('Enable FeatureHub to send Slack messages',
+                    style: Theme.of(context).textTheme.titleSmall),
+                controlAffinity: ListTileControlAffinity.leading,
+                children: [
+                  if (!config.any((cfg) => cfg.key == 'slack.enabled'))
+                    const Row(
+                      children: [
+                        Text(
+                            "You are required to configure encryption key/password in the FeatureHub system properties file to enable Slack integration"),
+                        SizedBox(
+                          width: 16.0,
+                        ),
+                        FHExternalLinkWidget(
+                          tooltipMessage: "View documentation",
+                          link:
+                              "https://docs.featurehub.io/featurehub/latest/configuration.html#_encryption",
+                          icon: Icon(Icons.arrow_outward_outlined),
+                          label: 'Encryption key documentation',
+                        )
+                      ],
+                    ),
+                  if (config.any((cfg) => cfg.key == 'slack.enabled'))
                     Card(child: SlackSystemConfigWidget(knownConfigs: config))
-                  ],
-                )
+                ],
+              )
             ],
           );
         });
