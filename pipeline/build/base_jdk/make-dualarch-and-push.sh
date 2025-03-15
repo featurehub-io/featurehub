bf26de9699e42c388a222e2bcfd84617098edd4d
Richard Vowles <richard@bluetrainsoftware.com>
2025-03-16 12:59:23 +1300

    update base image

--- pipeline/build/base_jdk/make-dualarch-and-push.sh
+++ pipeline/build/base_jdk/make-dualarch-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #docker buildx create --use --config buildtoml --name
 docker buildx create --use  --name party-builder
-docker buildx build $DOCKER_OPTS --platform linux/amd64,linux/arm64 . -t featurehub/base_jdk:1.13 --push
+docker buildx build $DOCKER_OPTS --platform linux/amd64,linux/arm64 . -t featurehub/base_jdk:1.15 --push
 docker buildx rm party-builder
--- pipeline/build/base_jdk/make-image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build --platform linux/amd64 -t featurehub/base_jdk:1.13 .
