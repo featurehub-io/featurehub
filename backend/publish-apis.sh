26936b5e41c2a1eb9dbe10b7451758ded4c8a2ab
Richard Vowles <richard@bluetrainsoftware.com>
2025-07-22 20:04:21 +1200

    updated published apis

--- backend/publish-apis.sh
+++ backend/publish-apis.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cd ..
 set -euxo pipefail
-find . -name 'pom.xml' | xargs grep -l maven-openapi-publisher | xargs -L 1 -I % sh -c "mvn -Dapp-release=true -f % initialize || exit 255"
+mvn -Dapp-release=true -f backend/messaging-api/pom.xml initialize
+mvn -Dapp-release=true -f backend/edge-api/pom.xml initialize
+mvn -Dapp-release=true -f backend/mr-api/pom.xml initialize
+mvn -Dapp-release=true -f backend/enricher-api/pom.xml initialize
+#find . -name 'pom.xml' | xargs grep -l maven-openapi-publisher | xargs -L 1 -I % sh -c "mvn -Dapp-release=true -f % initialize || exit 255"
