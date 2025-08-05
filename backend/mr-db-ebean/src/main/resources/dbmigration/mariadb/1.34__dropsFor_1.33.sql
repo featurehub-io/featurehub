93599634a466ee5231c74c485393217097f1b319
Richard Vowles <richard@bluetrainsoftware.com>
2025-08-05 21:09:56 +1200

    fix mariadb and mysql index drops

--- backend/mr-db-ebean/src/main/resources/dbmigration/mariadb/1.34__dropsFor_1.33.sql
+++ backend/mr-db-ebean/src/main/resources/dbmigration/mariadb/1.34__dropsFor_1.33.sql
@@ -1,4 +1,5 @@
 -- apply alter tables
+drop index ix_fh_organization_fk_named_cache on fh_organization;
 alter table fh_organization drop column fk_named_cache;
 -- apply post alter
 drop table if exists fh_cache;
