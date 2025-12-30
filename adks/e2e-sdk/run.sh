2d5d9912a134b0811505abfea065e24f53db9cbe
Richard Vowles <richard@bluetrainsoftware.com>
2025-12-30 16:22:23 +1300

    e2e updates for using with pnpm

--- adks/e2e-sdk/run.sh
+++ adks/e2e-sdk/run.sh
@@ -4,9 +4,9 @@ rm -rf logs/*
 if [ $# -eq 0 ]
   then
   echo DEBUG=true npm run test
-  DEBUG=true npm run test
+  DEBUG=true pnpm run test
 else
   echo DEBUG=true npm run test -- --tags $1
-  DEBUG=true npm run test -- --tags $1
+  DEBUG=true pnpm run test -- --tags $1
 fi
 
