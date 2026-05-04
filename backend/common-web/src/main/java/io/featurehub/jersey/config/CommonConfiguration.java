2d8b6e4ca609efeb93f528e869a93cc97ff91fd5
Richard Vowles <richard@bluetrainsoftware.com>
2026-05-04 13:54:13 +1200

    start enabling compression

--- backend/common-web/src/main/java/io/featurehub/jersey/config/CommonConfiguration.java
+++ backend/common-web/src/main/java/io/featurehub/jersey/config/CommonConfiguration.java
@@ -2,6 +2,7 @@ package io.featurehub.jersey.config;
 
 import cd.connect.openapi.support.OpenApiEnumProvider;
 import io.featurehub.jersey.OffsetDateTimeQueryProvider;
+import io.featurehub.jersey.SSEAwareEncodingFilter;
 import io.featurehub.lifecycle.LifecycleListenerFeature;
 import io.featurehub.rest.WebHeaderAuditLogger;
 import io.featurehub.utils.FallbackPropertyConfig;
@@ -15,6 +16,8 @@ import org.glassfish.jersey.message.GZipEncoder;
 /**
  * This class is used in clients and servers, so only classes that are relevant to
  * both should be registered here.
+ *
+ * It is registered in CommonFeatureHubFeatures, which is in turn registered in FeatureHubJerseyHost.
  */
 public class CommonConfiguration implements Feature {
 
@@ -23,6 +26,11 @@ public class CommonConfiguration implements Feature {
     config.property(CommonProperties.METAINF_SERVICES_LOOKUP_DISABLE, true);
     config.property(CommonProperties.FEATURE_AUTO_DISCOVERY_DISABLE, true);
     config.property(CommonProperties.MOXY_JSON_FEATURE_DISABLE, true);
+
+    if (!"true".equals(FallbackPropertyConfig.Companion.getConfig("http-compression-disable", "false"))) {
+      config.register(SSEAwareEncodingFilter.class);
+      config.register(GZipEncoder.class);
+    }
 
     // this is the objectmapper we want to use
     config.register(JacksonContextProvider.class);
